10177251b92e7b4e0730fd94518d7a8cb6c0b531
darasinghintel <dara.singh@intelligaia.com>
2021-06-18 15:03:06 +0530

    Added Banner Component

--- src/components/BannerCard.js
+++ src/components/BannerCard.js
@@ -24,56 +24,10 @@ function BannerCard(){
     const onClose = () => setOpen(false);
     return (
         <Grommet>
-            <Box pad="small" round="small" elevation="large" >
-                <Box fill align="center" justify="center">
-                    <Button icon={<Add color="brand" />} label={ <Text> <strong>Add</strong></Text>} onClick={onOpen} plain />
-                </Box>
-                {open && (
-                    <Layer
-                        position="bottom"
-                        modal={false}
-                        margin={{ vertical: 'medium', horizontal: 'small' }}
-                        onEsc={onClose}
-                        responsive={false}
-                        plain
-                    >
-                        <Box
-                            align="center"
-                            direction="row"
-                            gap="small"
-                            justify="between"
-                            round="xsmall"
-                            elevation="medium"
-                            background="white"
-                        //pad={{ vertical: 'xsmall', horizontal: 'small' }}
-                        >
-                            <Box align="center" direction="row" gap="xsmall">
-                                <Box pad="xsmall" background="status-ok" round={{ corner: 'left', size: 'xsmall' }}>
-                                    <StatusGood color='white' />
-                                </Box>
+            <Box pad="small" round="small" elevation="large" background="white" >
+         
 
-                                <Box pad="xsmall" background="white">
-                                    <Text>New Tag has been added successfully</Text>
-                                </Box>
-                            </Box>
-                            <Button icon={<FormClose />} onClick={onClose} plain />
-                        </Box>
-                    </Layer>
-                )}
-
-                {/* Spinner Block  Start*/}
-                <Box direction="row" align="center" alignSelf="center" justify="center" elevation="medium" width="small" height="medium" pad="xsmall" round="xsmall">
-                    <Spinner />
-                </Box>
-                {/* Spinner Block  Start*/}
-
-                {/* Spinner Block with Heading  Start*/}
-                <Box direction="column" align="center" alignSelf="center" justify="center" elevation="medium" width="medium" height="large" pad="xsmall" round="xsmall">
-                    <Spinner />
-                    <Paragraph margin="xsmall" pad="xsmall" size="xlarge">Creating New Host...</Paragraph>
-                </Box>
-                {/* Spinner Block with Heading  Start*/}
-
+                
                 {/* Header Starts */}
                 <Box
                     direction="row"
@@ -83,12 +37,14 @@ function BannerCard(){
                     alignSelf="" // Horizontal Align
                     justify="between" // Content Justification
                     alignContent="start"
+                    
+                    
                 >
-                    <Box pad="small" >
+                    <Box pad="xsmall" >
                         <Paragraph margin="none">Getting Started with Hosts</Paragraph>
                     </Box>
-                    <Box pad="small" >
-                        <Button plain icon={<MoreVertical />} onClick={() => { }} primary />
+                    <Box pad="xsmall" >
+                        <Button plain icon={<MoreVertical />} onClick={() => { }} />
                     </Box>
                 </Box>
                 {/* Header Ends */}
@@ -102,8 +58,9 @@ function BannerCard(){
                     alignSelf="" // Horizontal Align
                     justify="between" // Content Justification
                     alignContent="start"
+                    
                 >
-                    <Box pad="small" margin="small" width="55%"  >
+                    <Box pad="xsmall"  width="55%"  >
                         <Video  >
                             <source src="small.mp4" type="video/mp4" />
                             <source
@@ -120,13 +77,13 @@ function BannerCard(){
                             />
                         </Video>
                     </Box>
-                    <Box pad="small" width="45%" >
+                    <Box pad="xsmall" width="45%" >
                         <Paragraph size="xlarge" margin="none">We see you have not created any Host yet</Paragraph>
-                        <Paragraph margin="none">Get started by creating a new Host or explore the tutorials below.</Paragraph>
-                        <Box align="start" pad="none">
+                        <Paragraph size="medium" margin="none">Get started by creating a new Host or explore the tutorials below.</Paragraph>
+                        <Box align="start" pad="small"  margin={{ vertical: 'small', horizontal: 'none' }} >
                             <List data={locations} />
                         </Box>
-                        <Box align="start" pad="normal">
+                        <Box align="start" pad="normal" margin="small">
                             <Button label="Tell Me More" secondary />
                         </Box>
 
