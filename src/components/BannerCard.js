47d4e720e1af2e62472269ae8f946005d4e0f584
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2021-06-22 16:07:33 +0530

    banner card video link updated

--- src/components/BannerCard.js
+++ src/components/BannerCard.js
@@ -45,18 +45,9 @@ export default function BannerCard({ task: { disabled }}){
                 >
                     <Box pad="xsmall"  width="55%"  >
                         <Video  >
-                            <source src="small.mp4" type="video/mp4" />
                             <source
-                                src="http://techslides.com/demos/sample-videos/small.webm"
-                                type="video/webm"
-                            />
-                            <source
-                                src="http://techslides.com/demos/sample-videos/small.ogv"
-                                type="video/ogg"
-                            />
-                            <source
-                                src="http://techslides.com/demos/sample-videos/small.3gp"
-                                type="video/3gp"
+                                src="https://scrumbooster.s3.ap-south-1.amazonaws.com/sb/yt1s.com+-+Guest+Mode++Privacy+Actions.mp4"
+                                type="video/mp4"
                             />
                         </Video>
                     </Box>
